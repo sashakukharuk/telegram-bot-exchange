eff995484f54572f8afdb0327fe568404ec26100
Oleksandr <alexkukharuk@gmail.com>
2021-02-21 11:44:32 +0200

    correction

--- helpers/requestCurrency.js
+++ helpers/requestCurrency.js
@@ -1,5 +1,5 @@
 const axios = require('axios')
 
 module.exports.requestCurrency = async () => {
-    return await axios.get("https://api.exchangeratesapi.io/latest?base%DUSD").then(res => res.data)
+    return await axios.get("https://api.exchangeratesapi.io/latest?base=USD").then(res => res.data)
 }
--- logic/listCurrency.js
+++ logic/listCurrency.js
@@ -5,11 +5,7 @@ module.exports.listCurrency = async () => {
     const data = await requestCurrency()
     const rates = Object.entries(data.rates)
     rates.forEach(item => {
-        const different = 1/item[1]
-        listCurrency += `
-		${item[0]}: ${item[1].toFixed(2)} 
-		${item[0]}: ${item[1].toFixed(2)} USD: ${different.toFixed(2)}
-		`
+        listCurrency += `${item[0]}: ${item[1].toFixed(2)}\n`
     })
     return listCurrency
 }
